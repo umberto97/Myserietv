c0b4c17be4559564b0cc32edecde6c436ed654b7
umberto <umberto.gagliardini@studenti.unicampania.it>
2019-04-10 11:50:17 +0200

    Creo il View Holder e associo una variabile tvNome a ogni textview che è presente nel file riga.xml

---
+++ app/src/main/java/com/example/myserietv/ViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.myserietv
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlinx.android.synthetic.main.riga.view.*
+
+class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val tvTitolo = view.titolo_textview
+}

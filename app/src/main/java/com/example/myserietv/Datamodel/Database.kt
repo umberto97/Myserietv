5da94fe57c2fc0021182b277908ad32e22e1611b
umberto <umberto.gagliardini@studenti.unicampania.it>
2019-04-10 11:32:48 +0200

    Aggiungo campi e metodi al Database

--- app/src/main/java/com/example/myserietv/Datamodel/Database.kt
+++ app/src/main/java/com/example/myserietv/Datamodel/Database.kt
@@ -1,4 +1,21 @@
 package com.example.myserietv.Datamodel
 
 object Database {
+    //array che rappresenta le singole serie tv
+    private var array_serie = ArrayList<SerieTv>()
+    //quando viene istanziato un database esso eseguirà ciò che sta in init
+    init {
+        //inizialmente il database contiene una serietv
+        array_serie.add(SerieTv("The Punisher","Azione",2016))
+    }
+
+    // Restituisce l'elenco di tutte le serietv presenti
+    fun getElencoSerietv() : ArrayList<SerieTv> {
+        return array_serie
+    }
+
+    //aggiunge una nuova birra nel database
+    fun salvaSerietv(x : SerieTv) {
+        array_serie.add(x)
+    }
 }

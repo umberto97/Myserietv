982e6d91eb35ef2216d8251c096b3b043af2d616
umberto <umberto.gagliardini@studenti.unicampania.it>
2019-04-10 14:01:53 +0200

    Collego il figlio al padre e visualizzo nel fragment figlio le varie cose usando un bundle

--- app/src/main/java/com/example/myserietv/Adapter.kt
+++ app/src/main/java/com/example/myserietv/Adapter.kt
@@ -1,9 +1,11 @@
 package com.example.myserietv
 
 import android.content.Context
+import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.myserietv.Datamodel.SerieTv
 
 class Adapter(val dataset: ArrayList<SerieTv>,val context: Context) : RecyclerView.Adapter<ViewHolder>() {
@@ -23,5 +25,15 @@ class Adapter(val dataset: ArrayList<SerieTv>,val context: Context) : RecyclerVi
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val serietv = dataset.get(position)
         viewHolder.tvTitolo.text = serietv.titolo
+
+        // Imposto il listner per passare a visualizzare il dettaglio
+        viewHolder.itemView.setOnClickListener {
+
+            // Creo un bundle e vi inserisco la birra da visualizzare
+            val b = Bundle()
+            b.putParcelable("serietv",serietv)     //TODO: Il nome dell'ogggetto andrebbe inserito in un solo punto!!
+            Navigation.findNavController(it).navigate(R.id.action_padre_fr_to_fragment_figlio, b)
+        }
     }
+
 }
